46c611bded69a7a4b70f11fac92db168ddd00067
Anton <antonmylnikov@gmail.com>
2021-02-25 08:20:54 +0500

    Create README.md

--- alienInvasion/alien_invasion.py
+++ alienInvasion/alien_invasion.py
@@ -1,6 +1,7 @@
 import sys
 import pygame
 from settings import Settings
+from ship import Ship
 
 
 class AlienInvasion:
@@ -8,26 +9,36 @@ class AlienInvasion:
 
     def __init__(self):
         """Инициализирует игру и создает игровые ресурсы."""
+
         pygame.init()
         self.settings = Settings()
 
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
-
+        self.ship = Ship(self)
 
     def run_game(self):
         """Запуск основного цикла игры."""
+
         while True:
-            # Отслеживание событий клавиатуры и мыши.
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
+            self._check_events()
+            self._update_screen()
 
-            # При каждом проходе цикла перерисовывается экран.
-            self.screen.fill(self.settings.bg_color)
 
-            # Отображение последнего прорисованного икрана.
-            pygame.display.flip()
+    def _check_events(self):
+        """Обрабатывает нажатия клавиш и событтия мыши"""
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+
+    def _update_screen(self):
+        """Обнавляет изображения на экране и отображает новый экран"""
+
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+        pygame.display.flip()
 
 if __name__ == '__main__':
     # Создание экземпляра игры.
