990b37a7746cd82059bec2d99065654cdd87e51a
Anton <antonmylnikov@gmail.com>
2021-02-25 15:35:49 +0500

    Create README.md

--- alienInvasion/alien_invasion.py
+++ alienInvasion/alien_invasion.py
@@ -12,7 +12,6 @@ class AlienInvasion:
 
         pygame.init()
         self.settings = Settings()
-
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
         self.ship = Ship(self)
@@ -22,16 +21,21 @@ class AlienInvasion:
 
         while True:
             self._check_events()
+            self.ship.update()
             self._update_screen()
 
-
     def _check_events(self):
-        """Обрабатывает нажатия клавиш и событтия мыши"""
+        """Обрабатывает нажатия клавиш и события мыши"""
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
-
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = True
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
 
     def _update_screen(self):
         """Обнавляет изображения на экране и отображает новый экран"""
@@ -39,6 +43,7 @@ class AlienInvasion:
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
         pygame.display.flip()
+
 
 if __name__ == '__main__':
     # Создание экземпляра игры.
