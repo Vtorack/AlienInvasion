fd681afe715ac6ebc87ae2ae7d608a0e19c1d776
Anton <antonmylnikov@gmail.com>
2021-02-25 16:47:22 +0500

    Create README.md

--- alienInvasion/alien_invasion.py
+++ alienInvasion/alien_invasion.py
@@ -33,9 +33,22 @@ class AlienInvasion:
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RIGHT:
                     self.ship.moving_right = True
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True
+                elif event.key == pygame.K_UP:
+                    self.ship.moving_up = True
+                elif event.key == pygame.K_DOWN:
+                    self.ship.moving_down = True
+
             elif event.type == pygame.KEYUP:
                 if event.key == pygame.K_RIGHT:
                     self.ship.moving_right = False
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False
+                elif event.key == pygame.K_UP:
+                    self.ship.moving_up = False
+                elif event.key == pygame.K_DOWN:
+                    self.ship.moving_down = False
 
     def _update_screen(self):
         """Обнавляет изображения на экране и отображает новый экран"""
--- alienInvasion/ship.py
+++ alienInvasion/ship.py
@@ -6,6 +6,7 @@ class Ship():
     def __init__(self, ai_game):
         """Инициализирует корабль и задает его начальную позицую."""
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
 
         # Загружает изображение корабля и получает прямоугольник.
@@ -15,14 +16,34 @@ class Ship():
         # Каждый новый корабль появляется у нижнего края экрана.
         self.rect.midbottom = self.screen_rect.midbottom
 
+        # Сохранение вещественной координаты центра корабля.
+        self.x = float(self.rect.x)
+        self.y = float(self.rect.y)
+
         # Флаг перемещения корабля.
         self.moving_right = False
+        self.moving_left = False
+        self.moving_up = False
+        self.moving_down = False
 
 
     def update(self):
         """Обновляет позицию корабля с учетом флага."""
-        if self.moving_right:
-            self.rect.x += 1
+        # Обновляются атрибут х объекта ship, не rect.
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+
+        if self.moving_up and self.rect.top > 0:
+            self.y -= self.settings.ship_speed
+        if self.moving_down and self.rect.bottom < self.screen_rect.bottom:
+            self.y += self.settings.ship_speed
+
+        # Обнавление атрибута rect на основании self.x.
+        self.rect.x = self.x
+        self.rect.y = self.y
+
 
 
     def blitme(self):
